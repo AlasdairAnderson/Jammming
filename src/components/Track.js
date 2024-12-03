0628a4454f1b984b1fa1279e8103c64939751da1
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2024-12-03 15:55:31 +0000

    Initial static component

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
-function Track() {
+function Track(props) {
+    const [trackImg, setTrackImg] = useState('');
+    const [trackName, setTrackName] = useState('');
+    const [albumName, setAlbumName] = useState('');
+    const [artistName, setArtistName] = useState('');
 
+    return(
+        <div className="track">
+            <img className="track-img" src={trackImg}/>
+            <p className="track-name">{trackName}</p>
+            <p className="album-name">{albumName}</p>
+            <p classname="artist-name">{artistName}</p>
+            <button alt="Add track to playlist">Add Track</button>
+        </div>
+    )
 };
 
 export default Track;

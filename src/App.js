ffbb0193b7db94a66d40944f2d52127c16eb71be
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2024-12-03 16:18:44 +0000

    Update of initial React application to new components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchResults from './components/SearchResults';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Jammming</h1>
+        <p className="App-slogan">Search Spotify and build your ultimate playlist</p>
       </header>
+      <SearchResults />
     </div>
   );
 }

8649247ef1f39fbfbc5e1b6958fb624091a0de08
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2024-12-03 16:17:52 +0000

    Initail static component for SearchResults

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Searchbar from './Searchbar';
+import TrackList from './TrackList';
 
 function SearchResults() {
+
+    return(
+        <div className='searchResults'>
+            <Searchbar />
+            <TrackList />
+        </div>
+    );
 
 }
 

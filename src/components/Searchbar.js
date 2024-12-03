f7d5935b18ca06cc223ba71cc0ea1889dbed9c7f
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2024-12-03 16:08:50 +0000

    Initial static component for Searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Searchbar() {
+    const [searchTrack, setSearchTrack] = useState('');
+
+    const handleChange = (e) => {
+        setSearchTrack(e.target.value);
+    }
+
+    return(
+        <form>
+            <input id="searchTrack" name="searchTrack" type="text" onChange={handleChange} value={searchTrack}/>
+            <button id="submit" type="submit">Search</button>
+        </form>
+    );
 
 }
 
